442c46635c6328d50d4a5cbc12bc5102199c1903
Michael Patrick <patrm01@pfw.edu>
2025-11-05 14:32:05 -0500

    Interface started

--- Interface.java
+++ Interface.java
@@ -1,2 +1,9 @@
 public class Interface {
+
+    public int scanItem(int barcode){};
+    public void openCamera(){};
+    public void displayData(GroceryItem item){};
+    public String displayError(){};
+    public void applyFilter(int[] filters){};
+    public void displayScreen(){};
 }
