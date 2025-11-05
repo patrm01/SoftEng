0af5d11e428a0c74cfeae539ab0a981605652e7b
Jake <jakobpascute412@gmail.com>
2025-11-05 14:40:29 -0500

    Customer.java fields and getter/setters

--- Customer.java
+++ Customer.java
@@ -1,4 +1,22 @@
 public class Customer {
+	private String paymentInfo;
+	private int balance;
+	private String firstName, lastName;
+	private int customerAge;
+	
+	//defining boilerplate getters and setters
+	public int getBalance(){}
+	public void setBalance(int newBalance){}
 
+	public String getPaymentInfo(){}
+	public void setPaymentInfo(String paymentInfo){}
 
+	public String getFirstName(){}
+	public void setFirstName(String firstName){}
+
+	public String getLastName(){}
+	public void setLastName(String lastName){}
+
+	public int getCustomerAge(){}
+	public void setCustomerAge(int age){}
 }
