b50795f2f034c481a4ff17c68b9d906c539d8111
Michael Patrick <patrm01@pfw.edu>
2025-11-05 14:25:07 -0500

    groceryItem started

--- GroceryItem.java
+++ GroceryItem.java
@@ -1,10 +1,13 @@
 public class GroceryItem {
-private int itemId;
-private String name;
-private int stock;
-private double price;
-public GroceryItem(String name, int stock, double price, int itemID) {}
+    private int itemId;
+    private String name;
+    private int stock;
+    private double price;
 
-
-
+    public GroceryItem(String name, int stock, double price, int itemID) {
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.itemId = itemID;
+    }
 }
