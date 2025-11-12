31a6cc736966bce8bd0965d0b4ccfdc707bff3e1
SJCode <sjmihm04@gmail.com>
2025-11-12 14:43:47 -0500

    Modified classes Database and GroceryItem

--- Interface.java
+++ Interface.java
@@ -1,9 +1,21 @@
 public class Interface {
 
-    public int scanItem(int barcode){}
-    public void openCamera(){}
-    public void displayData(GroceryItem item){}
-    public String displayError(){};
-    public void applyFilter(int[] filters){}
-    public void displayScreen(){}
+    public int scanItem(int barcode){
+
+    }
+    public void openCamera(){
+
+    }
+    public void displayData(GroceryItem item){
+
+    }
+    public String displayError(){
+
+    };
+    public void applyFilter(int[] filters){
+
+    }
+    public void displayScreen(){
+
+    }
 }
