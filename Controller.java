e69a4ca7329b253ed8d28e23d802c9afd76b9b8e
Michael Patrick <patrm01@pfw.edu>
2025-11-13 22:08:11 -0500

    Major tweaks. Just need to add main for demo

--- Controller.java
+++ Controller.java
@@ -3,37 +3,50 @@ import java.util.ArrayList;
 public class Controller {
 
     private Database database;
+    private Customer customer;
 
-    public Controller() {
-        database = new Database();
+    public Controller(Customer customer) {
+        this.database = new Database();
+        this.customer = customer;
     }
 
     public ArrayList<GroceryItem> getData() {
         return database.getStoreList();
     }
 
-    public sendDatabase() {
-
+    // Just placeholder, will use to send data somewhere
+    public void sendDatabase() {
+        System.out.println("Database sent to interface.");
     }
 
-    public validateData() {
-
+    // Just placeholder, will use to validate customer or input
+    public boolean validateData() {
+        if (customer.getCustomerAge() <= 0) return false;
+        if (customer.getPaymentInfo() == null || customer.getPaymentInfo().length() != 12) return false;
+        return true;
     }
 
-    public sendInterface() {
-
+    // Just placeholder, will use to send something to interface
+    public void sendInterface() {
+        System.out.println("Interface updated.");
     }
 
-    public ArrayList<GroceryItem> filterItems(ArrayList<String> filters){
-        ArrayList<GroceryItem> items = new ArrayList<>();
-        if (filters == null || filters.size() == 0){
-            return items;
+    public ArrayList<GroceryItem> filterItems(ArrayList<String> filters) {
+        if (filters == null || filters.isEmpty()) {
+            return database.getStoreList();
         }
-        return items;
-
+        return database.getFilteredList(filters);
     }
 
     public ArrayList<GroceryItem> getCart() {
-        return cart;
+        return customer.getCart();
+    }
+
+    public void addToCart(GroceryItem item) {
+        customer.addToCart(item);
+    }
+
+    public void removeFromCart(GroceryItem item) {
+        customer.removeFromCart(item);
     }
 }
--- Database.java
+++ Database.java
@@ -2,38 +2,51 @@ import java.util.ArrayList;
 
 public class Database {
     private ArrayList<GroceryItem> storeList = new ArrayList<GroceryItem>();
-    storeList.add(new GroceryItem("milk", 20, 3.50, 0));
-    storeList.add(new GroceryItem("eggs", 100, 1.75, 1));
-    storeList.add(new GroceryItem("cheese", 25, 0.30, 2));
+    {
+        storeList.add(new GroceryItem("milk", 20, 3.50, 0));
+        storeList.add(new GroceryItem("eggs", 100, 1.75, 1));
+        storeList.add(new GroceryItem("cheese", 25, 0.30, 2));
+    }
 
     public void addNewItem(String name, int stockCount, double price, int itemID) {
-        storeList.add(new Groceryitem(name, stockCount, price, itemID));
+        storeList.add(new GroceryItem(name, stockCount, price, itemID));
     }
 
     public boolean removeItem(String itemName) {
         GroceryItem item = checkItemInDatabase(itemName);
-        if(storeList.remove(item) == true) {
-            return true;
+        if (item != null) {
+            return storeList.remove(item);
         }
         return false;
     }
 
     public boolean removeItem(GroceryItem item) {
-        if(storeList.remove(item) == true) {
-            return true;
-        }
-        return false;
+        return storeList.remove(item);
     }
 
     public GroceryItem checkItemInDatabase(String itemName) {
-        for(item : storeList) {
-            if(item.GetName().equals(itemName)) {
-                return(item);
+        for(GroceryItem item : storeList) {
+            if(item.getName().equals(itemName)) {
+                return item;
             }
         }
+        //has to have check for item not found
+        return null;
     }
+
 
     public ArrayList<GroceryItem> getStoreList() {
         return storeList;
+    }
+
+    public ArrayList<GroceryItem> getFilteredList(ArrayList<String> filters) {
+        ArrayList<GroceryItem> filteredList = new ArrayList<>();
+
+        for (GroceryItem item : storeList) {
+            if (filters.contains(item.getFilterFlag())) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
     }
 }
